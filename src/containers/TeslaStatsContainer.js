bcf68f94a768ab62e1dd5fccb0eebfc394c5a7a1
Josue Villanueva <idjevm@gmail.com>
2020-05-07 06:34:51 -0400

    aded actions/reducers and updated components and styling

---
+++ src/containers/TeslaStatsContainer.js
@@ -0,0 +1,11 @@
+import TeslaStats from '../components/TeslaStats/TeslaStats';
+import { connect } from 'react-redux'
+const mapStateToProps = (state) => {
+    return {
+        carstats: state.carstats
+    }
+}
+
+const TeslaStatsContainer = connect(mapStateToProps, null)(TeslaStats);
+
+export default TeslaStatsContainer;
---
+++ src/containers/TeslaTempCounterContainer.js
@@ -0,0 +1,27 @@
+import TeslaCounter from '../components/TeslaCounter/TeslaCounter';
+import { temperatureUp, temperatureDown } from '../actions/index';
+import { connect } from 'react-redux';
+import { counterDefaultVal } from '../constants/counterDefaultVal';
+
+const mapStateToProps = (state) => {
+    return {
+        currentValue: state.config.temperature,
+        initValues: counterDefaultVal.temperature
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        increment: (value) => {
+            dispatch(temperatureUp(value))
+        },
+        decrement: (value) => {
+            dispatch(temperatureDown(value))
+        }
+    }
+}
+
+const TeslaTempCounterContainer = connect(mapStateToProps, mapDispatchToProps)(TeslaCounter);
+
+export default TeslaTempCounterContainer;
+
